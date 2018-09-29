99dd5ac1e4f47d9c2b3d158dab3a6c1bd93a5ec8
zer0i3 <zer0i3@aliyun.com>
2018-09-29 09:06:25 +0800

    修改jenkins配置

--- scripts/docker_build_push.py
+++ scripts/docker_build_push.py
@@ -14,17 +14,15 @@ import traceback
 import codecs
 import threading
 import datetime
-import fnmatch
 
 CONCURRENT_THREAD_NUM = 4
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-log_file = "/tmp/docker_build.log"
+log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),'docker_build.log')
 if not os.path.exists(log_file):
-    codecs.open(log_file, 'a', encoding='utf-8').close()
-formatter = logging.Formatter(
-    "%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+    codecs.open(log_file, 'a',encoding='utf-8').close()
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
 root_log = logging.handlers.RotatingFileHandler(log_file, maxBytes=100000)
 console_log = logging.StreamHandler()
 root_log.setFormatter(formatter)
@@ -40,20 +38,22 @@ def list2dict(info_list):
         dict_info[info_key] = info[info_key]
     return dict_info
 
-
 def build_and_push(filepath):
     try:
-        print threading.currentThread().getName()
+        # print threading.currentThread().getName()
         logger.info("begin docker-compose build,push, filepath: %s" % filepath)
-        subprocess.check_call(['docker-compose', '-f', filepath, 'build'])
-        subprocess.check_call(['docker-compose', '-f', filepath, 'push'])
+        subprocess.check_call(['docker-compose','-f',filepath,'build'])
+        subprocess.check_call(['docker-compose','-f',filepath,'push'])
         logger.info("end docker-compose build,push, filepath: %s" % filepath)
-    except subprocess.CalledProcessError, e:
+    except subprocess.CalledProcessError,e:
         logger.error("Error occured when build image or push:" + filepath)
         raise e
 
 
+
 def handle_chal(chal, match_path):
+    #解决编码问题
+    match_path = match_path.decode('utf-8')
     # 获取题目名称
     chal_name = chal.keys()[0]
     chal_info = list2dict(chal[chal_name])
@@ -71,11 +71,11 @@ def handle_chal(chal, match_path):
             raise Exception
 
 
-def div_list(ls, n):
-	if not isinstance(ls, list) or not isinstance(n, int):
+def div_list(ls,n):
+	if not isinstance(ls,list) or not isinstance(n,int):
 		return []
 	ls_len = len(ls)
-	if n <= 0 or 0 == ls_len:
+	if n<=0 or 0==ls_len:
 		return []
 	# if n > ls_len:
 	# 	return []
@@ -83,16 +83,16 @@ def div_list(ls, n):
 		return [[i] for i in ls]
 	else:
 		ls_return = [[] for i in range(n)]
-        for i, v in enumerate(ls):
-            ls_return[i % n].append(v)
+        for i,v in enumerate(ls):
+            ls_return[i%n].append(v)
         return ls_return
-
 
 def handle_build_push(list):
     for env_path in list:
         build_and_push(env_path)
 
-
+@click.command()
+@click.argument('yml_path')
 def docker_update(yml_path):
     logger.info("begin to docker update:" + yml_path)
     start = datetime.datetime.now()
@@ -102,10 +102,10 @@ def docker_update(yml_path):
     if not os.path.isfile(yml_path):
         logger.error("path is not a file:" + yml_path)
         return
-    with codecs.open(yml_path, 'r', encoding='utf-8') as f:
+    with codecs.open(yml_path, 'r',encoding='utf-8') as f:
         try:
             matches = yaml.safe_load(f.read())
-        except yaml.YAMLError, e:
+        except yaml.YAMLError,e:
             logger.error('Error parsing YAML file:' + yml_path)
             raise e
         for match_name in matches:
@@ -121,45 +121,83 @@ def docker_update(yml_path):
                         # 获取具体题目信息
                         for chal in chals_in_cat:
                             try:
-                                result = handle_chal(
-                                    chal, os.path.dirname(yml_path))
+                                result = handle_chal(chal, os.path.dirname(yml_path))
                                 if result:
                                     all_env_list.append(result)
                             except Exception:
                                 has_exception = True
-                div_result = div_list(all_env_list, CONCURRENT_THREAD_NUM)
+                div_result = div_list(all_env_list,CONCURRENT_THREAD_NUM)
                 thread_list = []
                 for rs in div_result:
-                    t = threading.Thread(target=handle_build_push, args=(rs,))
+                    t = threading.Thread(target=handle_build_push,args=(rs,))
                     thread_list.append(t)
                 for t in thread_list:
                     t.start()
                 for t in thread_list:
                     t.join()
-
+                    
             except Exception, e:
                 logger.error("Error occured during docker update")
             else:
                 if not has_exception:
                     logger.info("docker update succeed in math:" + match_name)
                 else:
-                    logger.error("tracebacks exception detail:" +
-                                 traceback.format_exc())
+                    logger.error("tracebacks exception detail:" + traceback.format_exc())
     end = datetime.datetime.now()
     logger.info("cost time %f s" % (end - start).seconds)
 
 
-def find_files_under_path(path, file_match):
-    matches = []
-    for root, dirnames, filenames in os.walk(path):
-        for filename in fnmatch.filter(filenames, file_match):
-            matches.append(os.path.join(root, filename))
-    return matches
-
+# @click.command()
+# @click.argument('matches_path')
+def docker_update_all(matches_path):
+    all_env_list = []
+    for dirpath, dirnames, filenames in os.walk(matches_path):
+        for filepath in filenames:
+            if filepath == "data.yml":
+                try:
+                    yml_path = os.path.join(dirpath,'data.yml')
+                    with codecs.open(yml_path,'r',encoding='utf-8') as f:
+                        try:
+                            matches = yaml.safe_load(f.read())
+                        except yaml.YAMLError,e:
+                            logger.error('Error parsing YAML file:' + yml_path)
+                            raise e
+                        for match_name in matches:
+                            try:
+                                # 获取所有比赛
+                                has_exception = False
+                                match = matches[match_name]
+                                for match_info in match:
+                                    # 获取比赛中分类下的内容
+                                    if match_info != '_meta_':
+                                        chals_in_cat = match[match_info]
+                                        # 获取具体题目信息
+                                        for chal in chals_in_cat:
+                                            try:
+                                                result = handle_chal(chal, os.path.dirname(yml_path))
+                                                if result:
+                                                    all_env_list.append(result)
+                                            except Exception:
+                                                has_exception = True      
+                            except Exception, e:
+                                logger.error("Error occured during collect env path")
+                            else:
+                                if has_exception:
+                                    logger.error("tracebacks exception detail:" + traceback.format_exc())
+                except Exception,e:
+                    print path
+                    raise e
+    try:
+        div_result = div_list(all_env_list,CONCURRENT_THREAD_NUM)
+        thread_list = []
+        for rs in div_result:
+            t = threading.Thread(target=handle_build_push,args=(rs,))
+            thread_list.append(t)
+        for t in thread_list:
+            t.start()
+        for t in thread_list:
+            t.join()
+    except Exception:
+        logger.error("Error occured when build or push")
 if __name__ == '__main__':
-    current_root = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'../'))
-    # print current_root
-    data_yml_files = find_files_under_path(current_root, "data.yml")
-    for one_yml in data_yml_files:
-        print "[+] Phasing {}".format(one_yml)
-        docker_update(one_yml)
+    docker_update_all(os.path.dirname(os.path.dirname(__file__)))
