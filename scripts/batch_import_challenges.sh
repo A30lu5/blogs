68f729165f4693c08e4346a1008f11372e573772
kuraraneko <894729083@qq.com>
2018-10-30 16:44:23 +0800

    add oh-my-raddit1/2

--- scripts/batch_import_challenges.sh
+++ scripts/batch_import_challenges.sh
@@ -1,14 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 rootpath=`pwd`
 match_files=`find ./ -maxdepth 3 -name "data.yml"`
+
+# 保存好原来的IFS的值，方便以后还原回来
+PRE_IFS=$IFS
+
+# 设置IFS仅包括换行符
+IFS=$(echo -en "\n\b")
 
 for com_file in $match_files
 do
     echo "[+] Handing: $com_file"
     DIR=$(dirname $com_file)
-    . /home/ubuntu/env.sh && python $1 "$rootpath/$com_file"
+    # . /home/ubuntu/env.sh && 
+    python $1 "$rootpath/$com_file"
     echo "[+] $com_file have DONE"
 done
+
+IFS=$PRE_IFS
 
 echo "[+] Import All Matches DONE!"
